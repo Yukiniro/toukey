e3a220b8785c5ab6394b7b30fa158d501215a665
Zhanghao <yukiniro@hotmail.com>
2022-02-26 14:52:37 +0800

    update

---
+++ rollup.config.prod.js
@@ -0,0 +1,34 @@
+import { babel } from "@rollup/plugin-babel";
+import fileSize from "rollup-plugin-filesize";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "index.js",
+  output: [
+    {
+      file: "./dist/toukey.cjs.min.js",
+      format: "cjs"
+    },
+    {
+      file: "./dist/toukey.min.js",
+      format: "esm"
+    },
+    {
+      file: "./dist/toukey.esm.min.js",
+      format: "esm"
+    },
+    {
+      file: "./dist/toukey.umd.min.js",
+      format: "umd",
+      name: "toukey"
+    }
+  ],
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"]
+    }),
+    terser(),
+    fileSize()
+  ]
+};
