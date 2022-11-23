0d571b9b0d129c420bc62c493e0a2185515ddff1
Yukiniro <yukiniro@hotmail.com>
2022-11-24 07:50:52 +0800

    chore: change prettier config file to js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  endOfLine: "lf"
+};
