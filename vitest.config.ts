866740ee1c096ec0e77febc6be38eda9bf419a8f
Yukiniro <yukiniro@hotmail.com>
2023-12-19 21:00:28 +0800

    chore: update scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
-    testTimeout: 500
+    testTimeout: 500,
+    include: ["__test__/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      exclude: ["**/site/**", "**/playground/**", "src/types.ts"]
+    }
   }
 });
